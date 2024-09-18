c777bc082bc2f74a9aa017e92447b93c6933f4cd
BRACHEMI MEFTAH Hanene <68550699+hbrachemi@users.noreply.github.com>
2024-09-18 15:05:31 +0200

    Update loss_utils.py

--- loss_utils.py
+++ loss_utils.py
@@ -3,48 +3,63 @@ import torch.nn.functional as F
 from model_utils import encoder_QKV
 
 
-def self_attention_MH(layer_output_q, layer_output_k, layer_output_v, num_heads=16, dropout_rate=0):
+def self_attention_MH(layer_output_q, layer_output_k, layer_output_v,layer_qkv = None, num_heads=16, dropout_rate=0):
     """Using multi-head official implementation"""
-    # Determine batch size, sequence length, and embed_dim from the layer_output_q shape
-    batch_size, seq_len, embed_dim = layer_output_q.size()
+    if layer_output_q is not None and layer_output_k is not None:
+        # Determine batch size, sequence length, and embed_dim from the layer_output_q shape
+        batch_size, seq_len, embed_dim = layer_output_q.size()
     
-    # Calculate head_dim
-    head_dim = embed_dim // num_heads
-    if embed_dim % num_heads != 0:
-        raise ValueError("Embedding dimension must be divisible by the number of heads.")
+        # Calculate head_dim
+        head_dim = embed_dim // num_heads
+        if embed_dim % num_heads != 0:
+            raise ValueError("Embedding dimension must be divisible by the number of heads.")
     
-    # Reshape query, key, value to (batch_size, num_heads, seq_len, head_dim)
-    layer_output_q = layer_output_q.view(batch_size, seq_len, num_heads, head_dim).transpose(1, 2)
-    layer_output_k = layer_output_k.view(batch_size, seq_len, num_heads, head_dim).transpose(1, 2)
-    if layer_output_v is not None:
-        layer_output_v = layer_output_v.view(batch_size, seq_len, num_heads, head_dim).transpose(1, 2)
+        # Reshape query, key, value to (batch_size, num_heads, seq_len, head_dim)
+        layer_output_q = layer_output_q.view(batch_size, seq_len, num_heads, head_dim).transpose(1, 2)
+        layer_output_k = layer_output_k.view(batch_size, seq_len, num_heads, head_dim).transpose(1, 2)
+        if layer_output_v is not None:
+            layer_output_v = layer_output_v.view(batch_size, seq_len, num_heads, head_dim).transpose(1, 2)
+            value = layer_output_v.contiguous().view(batch_size * num_heads, seq_len, head_dim)
+        # Reshape them into (batch_size * num_heads, seq_len, head_dim)
+        query = layer_output_q.contiguous().view(batch_size * num_heads, seq_len, head_dim)
+        key = layer_output_k.contiguous().view(batch_size * num_heads, seq_len, head_dim)
 
-    # Reshape them into (batch_size * num_heads, seq_len, head_dim)
-    query = layer_output_q.contiguous().view(batch_size * num_heads, seq_len, head_dim)
-    key = layer_output_k.contiguous().view(batch_size * num_heads, seq_len, head_dim)
-    if layer_output_v is not None:
-        value = layer_output_v.contiguous().view(batch_size * num_heads, seq_len, head_dim)
+    if layer_qkv is not None:
+        batch_size, seq_len, concat_embed_dim = layer_qkv.size()
+        embed_dim = concat_embed_dim //3
+        head_dim = embed_dim // num_heads
+        layer_qkv = layer_qkv.reshape(batch_size, seq_len, 3, num_heads, embed_dim // num_heads).permute(2, 0, 3, 1, 4)
+        query, key, value = layer_qkv[0], layer_qkv[1], layer_qkv[2]
 
+    
     # Scaling factor to prevent large dot products
     scale = head_dim ** -0.5
     
     # Compute attention scores: (batch_size * num_heads, seq_len, seq_len)
-    attention_scores = torch.bmm(query, key.transpose(1, 2)) * scale
+    if layer_output_q is not None and layer_output_k is not None:
+        attention_scores = torch.bmm(query, key.transpose(1, 2)) * scale
+        # Apply softmax to get attention weights
+        attention_weights = F.softmax(attention_scores, dim=-1)
+        # Apply dropout (if applicable)
+        attention_weights = F.dropout(attention_weights, p=dropout_rate, training=True)
+        if value is None:
+            return None, attention_weights
+        # Compute the attention output: (batch_size * num_heads, seq_len, head_dim)
+        proj_result = torch.bmm(attention_weights, value)
+        # Reshape the output back to (batch_size, seq_len, embed_dim)
+        proj_result = proj_result.view(batch_size, num_heads, seq_len, head_dim)
+        proj_result = proj_result.transpose(1, 2).contiguous().view(batch_size, seq_len, embed_dim)
     
-    # Apply softmax to get attention weights
-    attention_weights = F.softmax(attention_scores, dim=-1)
+    if layer_qkv is not None:
+        attention_scores = torch.matmul(query, key.transpose(-1, -2)) * scale
+        # Apply softmax to get attention weights
+        attention_weights = F.softmax(attention_scores, dim=-1)
+        # Apply dropout (if applicable)
+        attention_weights = F.dropout(attention_weights, p=dropout_rate, training=True)
+        proj_result = torch.matmul(attention_weights, value).permute(0, 2, 1, 3)
+        new_proj_result_shape = proj_result.size()[:-2] + (embed_dim,)
+        proj_result = proj_result.reshape(new_proj_result_shape)
     
-    # Apply dropout (if applicable)
-    attention_weights = F.dropout(attention_weights, p=dropout_rate, training=True)
-    if layer_output_v is None:
-        return None, attention_weights
-        
-    # Compute the attention output: (batch_size * num_heads, seq_len, head_dim)
-    proj_result = torch.bmm(attention_weights, value)
-    
-    # Reshape the output back to (batch_size, seq_len, embed_dim)
-    proj_result = proj_result.view(batch_size, num_heads, seq_len, head_dim)
-    proj_result = proj_result.transpose(1, 2).contiguous().view(batch_size, seq_len, embed_dim)
     
     return proj_result, attention_weights
 
