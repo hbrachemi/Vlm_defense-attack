fc5f3b845558272426e586efe77a024c202ac3d9
BRACHEMI MEFTAH Hanene <68550699+hbrachemi@users.noreply.github.com>
2024-09-26 15:43:05 +0200

    Added PRM attack

--- sota.py
+++ sota.py
@@ -41,3 +41,81 @@ def generate_sota_targeted(image,p,model,processor,target_label=None,optimizer='
             im[0,c,:] += processor.image_processor.image_mean[c]
        
     return T.ToPILImage()(im[0])
+
+
+
+
+def generate_sota_untargeted_PRM(image,boxes,p,model,vlm,processor,patch_dim=14,optimizer='adam',lr=1e-3,steps=100,early_stop=5,perturbation_budget=None):
+
+    
+    inputs = processor(text = p, images = image, return_tensors="pt").to(model.device)
+    im = torch.nn.Parameter(inputs.pop('pixel_values').to(model.device), requires_grad=True)
+    optimizer = initialize_optimizer(optimizer,im,lr)
+
+    list_patches = get_target_patches(image,boxes,im.shape[-2],im.shape[-1],14)
+    means = processor.image_processor.image_mean
+    stds = processor.image_processor.image_std
+
+    device = model.device
+
+    #register clean features:
+    encoder = encoder_QKV(vlm,model)
+   
+    clean_features = {}
+    def get_clean_activation(name):
+                def hook_fn(module, input, output):
+                    clean_features[name]=output.detach()
+                return hook_fn
+        
+    list_hooks = []
+    for idx,layer in enumerate(encoder["encoder_mlp_norm"]):
+        list_hooks.append(layer.register_forward_hook(get_clean_activation(str(idx))))
+
+    model_output = model(**inputs,pixel_values=im)
+    
+    for i in range(len(encoder["encoder_mlp_norm"])):
+        list_hooks[i].remove()
+    
+    with tqdm(range(steps), total=steps) as pbar:
+        for step in pbar:
+            optimizer.zero_grad()
+            loss = torch.zeros(1).to(device)
+            
+            adv_features = {}
+            def get_adv_activation(name):
+                def hook_fn(module, input, output):
+                    adv_features[name]=output.requires_grad_(True)
+                return hook_fn
+        
+            list_hooks = []
+            for idx,layer in enumerate(encoder["encoder_mlp_norm"]):
+                list_hooks.append(layer.register_forward_hook(get_adv_activation(str(idx))))
+            
+            model_output = model(**inputs,pixel_values=im)
+    
+            for i in range(len(encoder["encoder_mlp_norm"])):
+                list_hooks[i].remove()
+            
+            for i in range(len(encoder["encoder_mlp_norm"])):
+                loss+= torch.nn.CosineEmbeddingLoss()(clean_features[str(i)][0,list_patches],adv_features[str(i)][0,list_patches],torch.ones(len(list_patches)).to(device))
+                #loss += F.cosine_similarity(clean_features[str(i)][0,list_patches],adv_features[str(i)][0,list_patches]).mean()
+            loss.backward()
+        
+            if im.grad is None:
+                raise Exception("image is not being updated, check gradients...")
+            
+            optimizer.step()
+            
+            #if perturbation_budget is not None:
+                #fill this later
+            for channel in range(3):
+                im[:,channel,:].data.clamp_((0-means[channel])/stds[channel], (1-means[channel])/stds[channel])
+            
+            pbar.set_postfix(loss=loss.item())  
+            
+    im = im.detach().clone()
+    for c in range(3):
+            im[0,c,:] *= processor.image_processor.image_std[c]
+            im[0,c,:] += processor.image_processor.image_mean[c]
+          
+    return T.ToPILImage()(im[0])
