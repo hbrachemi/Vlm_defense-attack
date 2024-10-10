388176ba219a3b89a2aa952cc3d98fb54102ff0d
BRACHEMI MEFTAH Hanene <68550699+hbrachemi@users.noreply.github.com>
2024-10-10 22:22:37 +0200

    Update attack_utils.py

--- attack_utils.py
+++ attack_utils.py
@@ -57,7 +57,7 @@ def initialize_optimizer(optim_name,image,lr):
 
 import torch
 import torchvision.transforms as T
-
+import os
 def depatchify_fuyu_image(patches, original_image_shape, patch_size) -> "torch.Tensor":
         """
         Convert patchified image into a 3-channeled tensor image.
@@ -134,7 +134,10 @@ def evaluate_image(model,processor,label,path,path_img,kw_args=None,other_prompt
                         else:
                             prompt = f"USER: <image> \nIs there any {label} in the image?\nASSISTANT:"
                             if vlm == 'instruct_blip':
-                                prompt = f"Is there any {label} apparent in the image?"                        
+                                prompt = f"Is there any {label} apparent in the image?"  
+
+                            if vlm == 'Fuyu':
+                                prompt = f"Is there any {label} in this image?\n"
                             inputs = processor(text = prompt, images = image, return_tensors="pt").to(model.device)
                             try:
                                 inputs = {key: tensor.to(model.device) for key, tensor in inputs.items()}
@@ -166,20 +169,22 @@ def evaluate_image(model,processor,label,path,path_img,kw_args=None,other_prompt
                             if vlm == 'instruct_blip':
                                 prompt = prompt.replace('USER: <image> ','').replace('ASSISTANT:','').replace("\n","")
                             if vlm == 'Fuyu':
-                                prompt = prompt.replace('USER: <image> ','').replace('ASSISTANT:','')
+                                prompt = "Offer a short description of the subjects present in this image.\n"
                             inputs = processor(text = prompt, images = image, return_tensors="pt").to(model.device)
                             try:
                                 inputs = {key: tensor.to(model.device) for key, tensor in inputs.items()}
                             except:
                                 pass
-                            model_output = model.generate(**inputs,max_length=1000,output_scores= True,return_dict_in_generate=True)
+                            model_output = model.generate(**inputs,max_length=200,output_scores= True,return_dict_in_generate=True)
                             file.write(f"{processor.decode(model_output.sequences[0])}\n\n\n")
                             file.flush()
-
-                            if other_prompts is not None:
+                            
+                            if other_prompts is not None and vlm != "Fuyu":
                                 for p in other_prompts:
                                     if vlm == 'instruct_blip':
                                         p = p.replace('USER: <image> ','').replace('ASSISTANT:','').replace("\n","")
+                                    if vlm == "Fuyu":
+                                        p = p.replace('USER: <image> ','').replace('ASSISTANT:','')
                                     inputs = processor(text = p, images = image, return_tensors="pt").to(model.device)
                                     try:
                                         inputs = {key: tensor.to(model.device) for key, tensor in inputs.items()}
@@ -223,6 +228,7 @@ def plot_losses(losses,save_loss=True,path = None):
         plt.show()
 
 def check_model_recognition(model,processor,image,label,vlm):
+
     if vlm == "CLIP":
             pos_prompt = f"An image of a {GT_data[label[0]][0]}"
             neg_prompt = f"Not an image of a {GT_data[label[0]][0]}"
@@ -242,7 +248,7 @@ def check_model_recognition(model,processor,image,label,vlm):
     elif vlm =='Blip-2':
         prompt = f"Question: are there any {GT_data[label[0]]} in this image? Answer:"
     elif vlm == "Fuyu":
-        prompt = f"Are there any {GT_data[label[0]]} in this image?\n"
+        prompt = f"Is there any {GT_data[label[0]]} in this image?\n"
 
 
     inputs = processor(text = prompt, images = image, return_tensors="pt").to(model.device)
@@ -253,23 +259,13 @@ def check_model_recognition(model,processor,image,label,vlm):
 from torchvision import transforms
 
 def check_attack_convergence(model,processor,image,label,vlm,id=None,patchified=False,original_image_shape=None,patch_size=(30,30)):
+    
     pil_image = torch.clone(image)
-    if patchified:
-        pil_image = depatchify_fuyu_image(pil_image.clone(), original_image_shape, patch_size)
-
-    stds = processor.image_processor.image_std
-    means = processor.image_processor.image_mean
-    if isinstance(means, float):
-            means = [means, means, means]
-    if isinstance(stds, float):
-            stds = [stds, stds, stds]
-
-    for c in range(3):
-            pil_image[0,c,:] *= stds[c]
-            pil_image[0,c,:] += means[c]
-        
-    pil_image =  transforms.ToPILImage()(pil_image[0])
-
+    
+    save_image(pil_image,f"./{id}.png",normalized=True,processor=processor,patchified=patchified,original_image_shape=original_image_shape,patch_size=patch_size)
+    
+    pil_image = Image.open(f"./{id}.png")
+    
     if vlm == "CLIP":
             pos_prompt = f"An image of a {GT_data[label[0]][0]}"
             neg_prompt = f"Not an image of a {GT_data[label[0]][0]}"
@@ -292,35 +288,44 @@ def check_attack_convergence(model,processor,image,label,vlm,id=None,patchified=
     inputs = processor(text = prompt, images = pil_image, return_tensors="pt").to(model.device)
     model_output = model.generate(**inputs,max_new_tokens =1)
     model_output = processor.decode(model_output[0])
-    return "No" in model_output or "no" in model_output or "</s>No" in model_output or "</s>no" in model_output
+    bool_no = "No" in model_output or "no" in model_output or "</s>No" in model_output or "</s>no" in model_output
+    bool_yes = "Yes" in model_output or "yes" in model_output
+    os.remove(f"./{id}.png")
+    return  bool_no or (not bool_yes)
     
 from losses import *
 from tqdm import tqdm
 import time
 
-def generate_adv_image_(image,label,boxes,model,processor,optimizer,lr,target_layers,lambda_a=1,lambda_e=0,lambda_n=0,lambda_p=0,lambda_pre_proj=0,w=336,h=336,patch_dim=14,steps=1000,checkpoint=100,path='./',img_name='test',att='mean',early_stop=5,check_convergence_rate=100,vlm='Llava-7b'):
+def generate_adv_image_(image,label,boxes,model,processor,optimizer,lr,p_budget=1,targeted_block=-1,lambda_a=1,lambda_e=0,lambda_n=0,lambda_p=0,lambda_pre_proj=0,w=336,h=336,patch_dim=14,steps=1000,num_heads=None,checkpoint=100,path='./',img_name='test',att='mean',early_stop=5,check_convergence_rate=100,vlm='Llava-7b'):
+    original_image_shape = (1,3,image.size[1],image.size[0])
+    encoder = encoder_QKV(vlm,model)
     
-    if not target_layers["qkv_state"]: #In some architectures qkv are projected using separated layers such as Llava
-        target_layers_q = target_layers["q"]
-        target_layers_k = target_layers["k"]
-        target_layers_v = target_layers["v"]
-        target_layers_proj = target_layers["proj"]  
-    
-    if target_layers["qkv_state"]: #In some architectures such as Insctruct-Blip qkv projections are done using one projection layer, the projections are thus concatenated and needs to be separated for future use
-        target_layers_qkv = target_layers["qkv"]
-        target_layers_proj = target_layers["proj"]
-        
-
-    
-    list_patches = get_target_patches(image,boxes,w,h,patch_dim)
-    means = processor.image_processor.image_mean
-    stds = processor.image_processor.image_std
     if vlm =='Fuyu':
-        inputs = processor(text = "An image of:\n", images = image, return_tensors="pt").to(model.device)
+        inputs = processor(text = "An image of:\n", images = image).to(device)
+        
         im = torch.nn.Parameter(inputs.pop('image_patches')[0].to(model.device), requires_grad=True)
+        patchified = True
+        input_ids = inputs["input_ids"]
+        patch_ids = inputs["image_patches_indices"]
+        im_id = "image_patches"
+
     else:
+        patchified = False
         inputs = processor(text = "USER: <image>\nASSISTANT:", images = image, return_tensors="pt").to(model.device)
-        im = torch.nn.Parameter(inputs.pop('pixel_values').to(model.device), requires_grad=True)
+        im = torch.nn.Parameter(inputs.pop("pixel_values").to(model.device), requires_grad=True)        
+        input_ids = None
+        patch_ids = None
+        im_id = "pixel_values"
+
+    list_patches = list(get_target_patches(image,boxes,w,h,patch_dim,input_ids,patch_ids))
+    means = processor.image_processor.image_mean
+    if not isinstance(means, (list, tuple)):
+        means = [means, means, means]
+    stds = processor.image_processor.image_std
+    if not isinstance(stds, (list, tuple)):
+        stds = [stds, stds, stds]
+
     optimizer = initialize_optimizer(optimizer,im,lr)    
     
     loss_hist = []
@@ -331,16 +336,14 @@ def generate_adv_image_(image,label,boxes,model,processor,optimizer,lr,target_la
     loss_hist_proj = []
     
     if att == 'mean':
-        att_loss = CustomMHAttentionLoss(list_patches,target_layers["qkv_state"])
-    if att in ["ce","CE"]:
-        att_loss = CustomMHCEAttentionLoss(list_patches,target_layers["qkv_state"])
+        att_loss = CustomMHAttentionLoss(list_patches)
     
     entropy_loss = CustomEntropyLoss(target_token_indices=list_patches)
 
     start = time.time()
     early_stopping = EarlyStopping(patience=early_stop, delta=0.001)
 
-    save_image(im,f"{path}/adv_img/{img_name}_step_{0}.png",normalized=True,processor=processor)
+    save_image(im,f"{path}/adv_img/{img_name}_step_{0}.png",normalized=True,processor=processor,patchified = patchified,original_image_shape=original_image_shape,patch_size=(patch_dim,patch_dim))
     evaluate_image(model,processor,GT_data[label[0]],f"{path}/predictions/{img_name}_step_{0}.txt",f"{path}/adv_img/{img_name}_step_{0}.png",vlm=vlm)
     init_im = torch.clone(im)
 
@@ -348,85 +351,69 @@ def generate_adv_image_(image,label,boxes,model,processor,optimizer,lr,target_la
     for step in tqdm(range(steps)):
             epoch_loss = 0
             optimizer.zero_grad()            
-            activations = {}    
+            model_activations = {}    
                         
             def get_activation(name):
                 def hook_fn(module, input, output):
-                    activations[name]=output
+                    model_activations[name]=output
                 return hook_fn
                 
             def get_input(name):
                 def hook_fn(module, input, output):
-                    activations[name]=input
+                    model_activations[name]=input
                 return hook_fn
-            loss_a = torch.zeros(1).to(model.device)
-            loss_e = torch.zeros(1).to(model.device)
-            loss_n = torch.zeros(1).to(model.device)
-            loss_p = torch.zeros(1).to(model.device)
-            loss_proj = torch.zeros(1).to(model.device)
-
-
-        
-            for l in range(len(target_layers_proj)):
-                    if not target_layers["qkv_state"]:
-                        hook_handle_v = target_layers_v[l].register_forward_hook(get_activation('V'))
-                        hook_handle_k = target_layers_k[l].register_forward_hook(get_activation('K'))
-                        hook_handle_q = target_layers_q[l].register_forward_hook(get_activation('Q'))
-                        hook_handle_proj = target_layers_proj[l].register_forward_hook(get_input('proj'))
-
-                        model_output = model(**inputs,pixel_values=im)
+            loss_a = torch.zeros(1)
+            loss_e = torch.zeros(1)
+            loss_n = torch.zeros(1)
+            loss_p = torch.zeros(1)
+            loss_proj = torch.zeros(1)
+            list_hooks = []
+            for l in range(targeted_block):
+                for l_key in list(encoder.keys()):
+                    list_hooks.append(encoder[l_key][l].register_forward_hook(get_activation(f"{l_key}_{l}")))
+                    list_hooks.append(encoder[l_key][l].register_forward_hook(get_input(f"{l_key}_{l}_input")))
                     
-                        layer_output_v = activations['V']
-                        layer_output_k = activations['K']
-                        layer_output_q = activations['Q']
-                        layer_proj = activations['proj'][0]
-
-                        hook_handle_v.remove()                
-                        hook_handle_k.remove()                
-                        hook_handle_q.remove()                
-                        hook_handle_proj.remove()   
+            if vlm == "Fuyu":
+                model_output = model(**inputs,image_patches=im)
+            else:
+                model_output = model(**inputs,pixel_values=im)
                 
-                        if lambda_a!=0:
-                            loss_a += att_loss(layer_output_q, layer_output_k)
-                        if lambda_e !=0:
-                            loss_e += entropy_loss(layer_output_v)
-                        if lambda_n !=0:
-                            loss_n += layer_output_v[0][list_patches].norm(dim=1).mean()
-                        if lambda_pre_proj!=0:
-                            loss_proj += layer_proj[0][list_patches].norm(dim=1).mean()
-                        
-                        
-                    if target_layers["qkv_state"]:
-                        hook_handle_qkv = target_layers_qkv[l].register_forward_hook(get_activation('qkv'))
-                        hook_handle_proj = target_layers_proj[l].register_forward_hook(get_input('proj'))
-                        
-                        model_output = model(**inputs,pixel_values=im)
-
-                        layer_output_qkv = activations['qkv']
-                        layer_proj = activations['proj'][0]
-                        _ , _ , value = extract_q_k_v_from_qkv(layer_output_qkv,vlm=vlm)
-                        
-                        hook_handle_qkv.remove()   
-                        hook_handle_proj.remove() 
-
-                        if lambda_a!=0:
-                            loss_a += att_loss(None, None,layer_output_qkv)
-                        if lambda_e !=0:
-                            loss_e += entropy_loss(value)
-                        if lambda_n !=0:
-                            loss_n += value[:,:,list_patches,:].norm()
-                        if lambda_pre_proj!=0:
-                            loss_proj += layer_proj[0][list_patches].norm(dim=1).mean()
+            for i in range(len(list_hooks)):
+                list_hooks[i].remove()                
+            
+            for l in range(targeted_block):
+                block_acti = {}
+                for l_key in list(encoder.keys()):
+                    try:
+                        block_acti[l_key] = model_activations[f"{l_key}_{l}"]
+                        block_acti[f"{l_key}_input"] = model_activations[f"{l_key}_{l}_input"]
+                    except:
+                        pass
+                if lambda_a!=0:
+                            loss_a += att_loss(block_acti,num_heads,dropout_rate=0, vlm = vlm, model = model, inputs = inputs).to(loss_a.device)
+                if 'V' in list(block_acti.keys()):
+                    layer_output_v = block_acti["V"]
+                else:
+                    query,key,layer_output_v = extract_q_k_v_from_qkv(block_acti["qkv"],num_heads = num_heads,vlm=vlm)
                 
+                if vlm =='Fuyu':
+                    layer_output_v = layer_output_v.permute(0,2,1,3)
+                if lambda_e !=0:
+                            loss_e += entropy_loss(layer_output_v).to(loss_a.device)
+                if lambda_n !=0:
+                                loss_n += layer_output_v[0,:,list_patches].norm(dim=1).mean().to(loss_a.device)
+                if lambda_pre_proj!=0:
+                            loss_proj += block_acti["proj"][0,:,list_patches].norm(dim=1).mean().to(loss_a.device)
+                        
                         
             if lambda_p !=0:
                 loss_p += (init_im - im).norm()
                         
             loss = lambda_a*loss_a + lambda_e*loss_e + lambda_n*loss_n + lambda_p*loss_p + lambda_pre_proj*loss_proj
-
-            loss.backward(retain_graph=True)
-            optimizer.step()
             
+            loss.backward(retain_graph=True)
+            
+            optimizer.step()
             if im.grad is None:
                 raise Exception("Image grad is None")
             
@@ -434,7 +421,8 @@ def generate_adv_image_(image,label,boxes,model,processor,optimizer,lr,target_la
 
             for channel in range(3):
                 im[:,channel,:].data.clamp_((0-means[channel])/stds[channel], (1-means[channel])/stds[channel])
-    
+                
+            im.data.clamp_(init_im - p_budget, init_im + p_budget)
             loss_hist.append(epoch_loss)
             loss_hist_a.append(loss_a.item())
             loss_hist_e.append(loss_e.item())
@@ -449,9 +437,9 @@ def generate_adv_image_(image,label,boxes,model,processor,optimizer,lr,target_la
             
             if (step+1) % checkpoint == 0 :
                 end = time.time()
-
+                    
                 kw_args = {"exec_time":end-start,"num_patches":len(list_patches)}
-                save_image(im,f"{path}/adv_img/{img_name}_step_{step+1}.png",normalized=True,processor=processor)
+                save_image(im,f"{path}/adv_img/{img_name}_step_{step+1}.png",normalized=True,processor=processor,patchified = patchified,original_image_shape=original_image_shape,patch_size=(patch_dim,patch_dim))
                 
                 evaluate_image(model,processor,GT_data[label[0]],f"{path}/predictions/{img_name}_step_{step+1}.txt",f"{path}/adv_img/{img_name}_step_{step+1}.png",kw_args,vlm=vlm)
 
@@ -471,12 +459,17 @@ def generate_adv_image_(image,label,boxes,model,processor,optimizer,lr,target_la
                     
                 start -= time.time()-end
             if (step+1) % check_convergence_rate == 0 :
-                if check_attack_convergence(model,processor,im,label,vlm):
+                if check_attack_convergence(model,processor,im.clone(),label,vlm,id=os.getpid(),patchified = patchified,original_image_shape=original_image_shape,patch_size=(patch_dim,patch_dim)):
                     break
             
     best_image = early_stopping.best_image
     end = time.time()
+    optimizer.zero_grad()
+    
+    del model_activations
+    del block_acti
+
     kw_args = {"exec_time":end-start,"num_patches":len(list_patches)}
-    save_image(im,f"{path}/best/{img_name}_best.png",normalized=True,processor=processor)
+    save_image(im,f"{path}/best/{img_name}_best.png",normalized=True,processor=processor,patchified = patchified,original_image_shape=original_image_shape,patch_size=(patch_dim,patch_dim))
     evaluate_image(model,processor,GT_data[label[0]],f"{path}/best/{img_name}_best.txt",f"{path}/best/{img_name}_best.png",kw_args,possible_prompts,vlm=vlm)
     return best_image, end-start, im
